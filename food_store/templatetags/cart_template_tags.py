79f7690cdbc3a1283201bb9850ae1e9c1b0d16cc
vbello-tech <odunayobello1@gmail.com>
2022-07-13 12:19:50 +0100

    cart

--- food_store/templatetags/cart_template_tags.py
+++ food_store/templatetags/cart_template_tags.py
@@ -8,6 +8,6 @@ def cart_item_count(user):
     if user.is_authenticated:
         qs = Order.objects.filter(user=user, ordered=False)
         if qs.exists():
-            return qs[0].items.count
+            return qs[0].foods.count
     else:
         return 0
